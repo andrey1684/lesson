0b9e2521c071c53d8d89a5a0556268aed417d764
Andrey1684 <andreysergeevich1684@gmail.com>
2024-10-12 15:46:39 +0300

    Initial commit

---
+++ new_project.py
@@ -0,0 +1,19 @@
+number = 285
+n1 = number // 100;
+n2 = number % 100 // 10;
+n3 = number % 100 % 10;
+result = n1 + n2 + n3;
+print(n1)
+print(n2)
+print(n3)
+print(result)
+
+number = 447
+n1 = number // 100;
+n2 = number % 100 // 10;
+n3 = number % 100 % 10;
+result = n1 + n2 + n3;
+print(n1)
+print(n2)
+print(n3)
+print(result)
