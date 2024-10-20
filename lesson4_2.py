aae2b86406f2b883562f3692de88fa0a2dfef77a
Andrey1684 <andreysergeevich1684@gmail.com>
2024-10-20 17:36:43 +0300

    lesson4_2

---
+++ lesson4_2.py
@@ -0,0 +1,14 @@
+import random
+numbers = []
+even = []
+for i in range(random.randint(10, 20)):
+    numbers.append(random.randint(1, 100))
+even = numbers[0: 20: 2]
+print(numbers)
+print(even)
+print(sum(even))
+
+numbers_2 = [random.randint(1, 100) for i in range(random.randint(10, 20))]
+even_2 = numbers_2[0: 20:2]
+print(numbers_2)
+print(sum(even_2))
