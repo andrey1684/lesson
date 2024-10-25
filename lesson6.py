527db5fc12764829ddca8a4ded81f6add6fa91b2
Andrey1684 <andreysergeevich1684@gmail.com>
2024-10-25 22:17:02 +0300

    lesson6

---
+++ lesson6.py
@@ -0,0 +1,24 @@
+import string
+first = input("first_symbol")
+second =input("second_symbol")
+elements = first, "-", second
+elements = list(elements)
+# abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+abc = string.ascii_letters
+# print("".join(elements))
+answer = []
+first = abc.find(first)
+second = abc.find(second)
+# print(first)
+# print(second)
+x = int(second) - int(first)
+if x < 0:
+    print("not correct range")
+answer = abc[first: int(second) + 1]
+print(answer)
+
+
+# "a-c" -> abc
+# "a-a" -> a
+# "s-H" -> stuvwxyzABCDEFGH
+# "a-A" -> abcdefghijklmnopqrstuvwxyzA
